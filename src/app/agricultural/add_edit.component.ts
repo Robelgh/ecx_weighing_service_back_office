5222c8efe850f4d99f79429ec7dfe75af3ea2ca2
Robelgh <robelghw123@gmail.com>
2023-08-11 15:15:46 +0300

    basic setup

---
+++ src/app/agricultural/add_edit.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { AbstractControlOptions, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { first } from 'rxjs/operators';
+
+import { UserService, AlertService } from '../_services';
+import { MustMatch } from '../_helper';
+
+
+@Component({ templateUrl: 'add_edit.component.html' })
+
+export class AddEditComponent implements OnInit{
+   
+    form!: FormGroup;
+    id!: string;
+    isAddMode!: boolean;
+    loading = false;
+    submitted = false;
+
+    constructor(
+        private formBuilder: FormBuilder,
+        private route: ActivatedRoute,
+        private router: Router,
+        private userService: UserService,
+        private alertService: AlertService
+    ) {}
+  }
+  
---
+++ src/app/agricultural/list.component.ts
@@ -0,0 +1,9 @@
+
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({ templateUrl: 'list.component.html' })
+export class ListComponent {
+}
+
+
